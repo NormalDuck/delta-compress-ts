ed3ba79ae3266f260bce5fa3af25731af29221ae
Saber <88921984+SaberClass@users.noreply.github.com>
2025-02-07 20:30:46 +0400

    Create index.d.ts + tuple type fix (should fix multiple return args)

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+export declare function applyImmutable(old: any, diff: buffer): any
+export declare function applyMutable(old: any, diff: buffer): any
+export declare function diffImmutable(old: any, _new: any): LuaTuple<[buffer, any]>
+export declare function diffMutable(old: any, _new: any): LuaTuple<[buffer, any]>
