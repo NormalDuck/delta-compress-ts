42f9923bb39089d9271e45e31569a511d919c684
duck.__index <138041510+NormalDuck@users.noreply.github.com>
2025-02-12 16:25:49 -0800

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
 export declare function applyImmutable(old: any, diff: buffer): any
 export declare function applyMutable(old: any, diff: buffer): any
-export declare function diffImmutable(old: any, _new: any): LuaTuple<[buffer, any]>
-export declare function diffMutable(old: any, _new: any): LuaTuple<[buffer, any]>
+export declare function diffImmutable(old: any, _new: any): buffer | undefined
+export declare function diffMutable(old: any, _new: any): LuaTuple<[buffer | undefined, any | undefined]>
